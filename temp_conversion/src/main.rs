85cf405ea8d5780171d23999a5ae6405d40d5a90
mieshiii <mieseliel.delacruz@gmail.com>
2023-08-18 23:40:16 +0800

    #17 - add inputs for celsius, incomplete

--- temp_conversion/src/main.rs
+++ temp_conversion/src/main.rs
@@ -1,17 +1,30 @@
 // import io lib
-use std::io;
+use std::io::{self, Read};
 
 fn main() {
-  println!("Please choose the input type:");
+    println!("Please choose the conversion: C or F.");
 
-  let mut input: char = char::new();
+    let mut input: [u8; 1] = [0u8; 1];
+    io::stdin().read_exact(&mut input).expect("Failed to read line");
+    let character: u8 = input[0];
   
-  if input == 'C' {
-      println!("Converting C to F");
-  } else if input == 'F' {
-      println!("Converting F to C");
-  } else {
-      println!("Invalid input!");
-  }
-  
+    if character == b'C' {
+        println!("Converting C to F");
+        println!("Input the value to convert:");
+        // input type is a String(?)
+        let mut val: String = String::new();
+        io::stdin().read_line(&mut val).expect("Failed to read line");
+
+        // convert String to float
+        let input_val: f64 = val.trim().parse().expect("Invalid input");
+        let converted_val: f64 = (input_val * 9.0/5.0) + 32.0;
+
+        println!("Converted value in F is:{}", converted_val);
+
+    } else if character == b'F' {
+        println!("Converting F to C");
+    } else {
+        println!("Invalid input!");
+    }
+    
 }
