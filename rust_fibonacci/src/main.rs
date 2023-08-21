e5db8814fbab09fad842026b2704ddc6e75a10bb
mieshiii <mieseliel.delacruz@gmail.com>
2023-08-21 23:07:26 +0800

    #20 - add input for rust_fib

--- rust_fibonacci/src/main.rs
+++ rust_fibonacci/src/main.rs
@@ -2,5 +2,11 @@
 use std::io;
 
 fn main() {
-    println!("Test");
+    println!("Input the nth number:");
+
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let input_val: f64 = input.trim().parse().expect("Invalid input");
+
+    println!("The number is {}", input_val);
 }
