8442d9cc6ca8e4e5ec0ca0f53531f012d54902c1
mieshiii <mieseliel.delacruz@gmail.com>
2023-08-23 23:59:27 +0800

    #21 - check logic for fib seq

--- rust_fibonacci/src/main.rs
+++ rust_fibonacci/src/main.rs
@@ -8,5 +8,9 @@ fn main() {
     io::stdin().read_line(&mut input).expect("Failed to read line");
     let input_val: f64 = input.trim().parse().expect("Invalid input");
 
+    // algo for the nth number in fib sequence
+    // loop here
+    //  logic here
+
     println!("The number is {}", input_val);
 }
